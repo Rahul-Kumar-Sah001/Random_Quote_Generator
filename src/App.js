9d8d7e2b2703d2ab2ea663ed52043ab08e7daf3c
Rahul <rahulsah7846@gmail.com>
2024-04-05 22:45:18 +0530

    I changed the logo image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from 'axios';
+import './App.css'
+class App extends React.Component{
+    state = {advice : ''};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Hello Rahul Here 
-      </header>
-    </div>
-  );
+    componentDidMount(){
+        this.fetchAdvice();
+    }
+    
+    fetchAdvice = ()=>{
+        axios.get('https://api.adviceslip.com/advice')
+
+        .then((response) => {
+            const {advice} = response.data.slip;
+            this.setState({advice});
+        })
+
+        .catch((error)=>{
+            console.log(error);
+        });
+    }
+    render(){
+        const {advice} = this.state;
+        return (
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">
+                        {advice}
+                    </h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>GIVE ME A QUOTE!</span>
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
